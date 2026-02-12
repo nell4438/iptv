7dbebcbd8eb64ebad00ee62253b5fad27e370075
nell4438 <98459805+nell4438@users.noreply.github.com>
2026-02-12 22:37:21 +0800

    Create h.js

---
+++ h.js
@@ -0,0 +1,104 @@
+[
+  {
+    "channel_name": "A2Z",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/cg_a2z/default/index.mpd",
+    "id_1": "3f6d8a2c1b7e4c9f8d52a7e1b0c6f93d",
+    "id_2": "4019f9269b9054a2b9e257b114ebbaf2"
+  },
+  {
+    "channel_name": "ABCAUSTRALIA",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/abc_aus/default/index.mpd",
+    "id_1": "d6f1a8c29b7e4d5a8f332c1e9d7b6a90",
+    "id_2": "790bd17b9e623e832003a993a2de1d87"
+  },
+  {
+    "channel_name": "ALJAZEERA",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/dr_aljazeera/default/index.mpd",
+    "id_1": "7f3d900a04d84492b31fe9f79ac614e3",
+    "id_2": "d33ff14f50beac42969385583294b8f2"
+  },
+  {
+    "channel_name": "ANIMALPLANET",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_animal_planet_sd/default/index.mpd",
+    "id_1": "1c9f7a6d3b2e4e5d8a61f4d0c2b9e813",
+    "id_2": "b8f52451c67a2b54f272543eef45b621"
+  },
+  {
+    "channel_name": "ANIMAX",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_animax_sd_new/default/index.mpd",
+    "id_1": "1e7b9d2c6a4f4d8c9f33b5c1a8d7e260",
+    "id_2": "67336c0c5b24fb4b8caac248dad3c55d"
+  },
+  {
+    "channel_name": "ARIRANG",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/arirang_sd/default/index.mpd",
+    "id_1": "13815d0fa026441ea7662b0c9de00bcf",
+    "id_2": "2d99a55743677c3879a068dd9c92f824"
+  },
+  {
+    "channel_name": "ASIANFOODNETWORK",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/asianfoodnetwork_sd/default/index.mpd",
+    "id_1": "1619db30b9ed42019abb760a0a3b5e7f",
+    "id_2": "5921e47fb290ae263291b851c0b4b6e4"
+  },
+  {
+    "channel_name": "AXN",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/cg_axn_sd/default/index.mpd",
+    "id_1": "8a6c2f1e9d7b4c5aa1f04d2b7e9c1f88",
+    "id_2": "05e6bfa4b6805c46b772f35326b26b36"
+  },
+  {
+    "channel_name": "BBCEARTH HD",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_bbcearth_hd1/default/index.mpd",
+    "id_1": "34ce95b60c424e169619816c5181aded",
+    "id_2": "0e2a2117d705613542618f58bf26fc8e"
+  },
+  {
+    "channel_name": "BBCWORLDNEWS",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/bbcworld_news_sd/default/index.mpd",
+    "id_1": "f59650be475e4c34a844d4e2062f71f3",
+    "id_2": "119639e849ddee96c4cec2f2b6b09b40"
+  },
+  {
+    "channel_name": "BILYONARYO CH",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/bilyonaryoch/default/index.mpd",
+    "id_1": "227ffaf09bec4a889e0e0988704d52a2",
+    "id_2": "b2d0dce5c486891997c1c92ddaca2cd2"
+  },
+  {
+    "channel_name": "BLOOMBERG",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/bloomberg_sd/default/index.mpd",
+    "id_1": "3b8e6d1f2c9a4f7d9a556c1e7b2d8f90",
+    "id_2": "09f0bd803966c4befbd239cfa75efe23"
+  },
+  {
+    "channel_name": "BLUEANTEXTREME",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/dr_rockextreme/default/index.mpd",
+    "id_1": "8d2a6f1c9b7e4c3da5f01e7b9c6d2f44",
+    "id_2": "23841651ebf49fa03fdfcd7b43337f87"
+  },
+  {
+    "channel_name": "CARTOONNET HD",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/cartoonnetworkhd/default/index.mpd",
+    "id_1": "a2d1f552ff9541558b3296b5a932136b",
+    "id_2": "cdd48fa884dc0c3a3f85aeebca13d444"
+  },
+  {
+    "channel_name": "CELESTIALMOVIEPINOY",
+    "url": "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/celmovie_pinoy_sd/default/index.mpd",
+    "id_1": "0f8537d8412b11edb8780242ac120002",
+    "id_2": "2ffd7230416150fd5196fd7ea71c36f3"
+  },
+  {
+    "channel_name": "CGTNENGLISH",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/cgtn/default/index.mpd",
+    "id_1": "0f854ee4412b11edb8780242ac120002",
+    "id_2": "9f2c82a74e727deadbda389e18798d55"
+  },
+  {
+    "channel_name": "CHANNELNEWSASIA",
+    "url": "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/channelnewsasia/default/index.mpd",
+    "id_1": "b259df9987364dd3b778aa5d42cb9acd",
+    "id_2": "753e3dba96ab467e468269e7e33fb813"
+  }
+]
